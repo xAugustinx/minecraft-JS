4e8e2c3359a6d7246494d00205cacfb6c7bcf1a2
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2025-12-09 22:15:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var bloki = [];
 
 
+var aktualneBloki = [];
+
+
 const blokiObracalne = ['txt/dirt.png'];
 
 for (let y = 0; y < 10; y++) {
@@ -14,13 +17,28 @@ var xRenderowania = 0;
 var xGracza = 4;
 var yGracza;
 
-function zrespBlok(source)
+
+var pointerX = 0;
+var pointerY = 0;
+
+
+
+function zrespBlok(source, y, x)
 {
-    const nE = document.createElement('img');
+    const nE = document.createElement('button');
+
+    nE.yMeow = y;
+    nE.xMeow = x;
+
+
 
     nE.src = source;
 
     nE.style.zIndex = 1;
+
+    nE.style.backgroundImage = 'url(' + source + ')';
+    nE.style.backgroundSize = 'contain';
+    nE.style.border = 'none';
 
     nE.style.position = 'absolute';
     nE.style.imageRendering = 'pixelated';
@@ -38,6 +56,7 @@ function zrespBlok(source)
         }
     }        
 
+
     return nE;
 }
 
@@ -46,18 +65,6 @@ function divBlokiNaZero()
     const mango = document.getElementById('bloki');
     mango.innerHTML = '';
 }
-
-function upadek()
-{
-    if (bloki[yGracza][xGracza] == -1)
-    {
-        yGracza++;
-        return true;
-    }
-    else { return false;}
-}
-
-
 
 function dodawanieDoDivaBlokow(mango, xBottom, xRight)
 {
@@ -73,6 +80,29 @@ function dodawanieDoDivaBlokow(mango, xBottom, xRight)
     nE.style.right = meowXright;
 
     document.getElementById("bloki").appendChild(nE);
+}
+
+function zmienianiePozycjiPointera(y,x)
+{
+    var myPointer = document.getElementById('pointer');
+
+    myPointer.style.visibility = 'visible';
+
+    var newX = x * -1 + 9;
+    var newY = y * -1 + 9;
+
+    myPointer.style.bottom = (newY * 10).toString() + '%';
+    myPointer.style.left = (newX * 10).toString() + '%';
+
+}
+
+function usuwaniePointera()
+{
+    var myPointer = document.getElementById('pointer');
+    myPointer.style.visibility = 'hidden';
+
+    xPointer = 4;
+    yPointer = yGracza;
 }
 
 
@@ -96,6 +126,7 @@ function generowanieSwiata()
     bloki[punktStartuTrawy][xRenderowania] = zrespBlok('txt/grass.png');
 
     var czyStone = false;
+    
 
     for (var i = punktStartuTrawy+1; i < 10; i++)
     {
@@ -106,10 +137,7 @@ function generowanieSwiata()
             if (losowaWartosc > 97) {bloki[i][xRenderowania] = zrespBlok('txt/coal.png');   }
             else {bloki[i][xRenderowania] = zrespBlok('txt/stone.png');}
         }
-        else
-        {
-            bloki[i][xRenderowania] = zrespBlok('txt/dirt.png');
-        } 
+        else { bloki[i][xRenderowania] = zrespBlok('txt/dirt.png'); } 
     }
     xRenderowania++;
 }
@@ -117,7 +145,6 @@ function generowanieSwiata()
 function renderowanie()
 {
     divBlokiNaZero();
-
 
     for (var y = 0; y < 10; y++)
     {
@@ -129,7 +156,6 @@ function renderowanie()
             }
         }
     }
-
     const gracz = document.getElementById('gracz');
 
     gracz.style.bottom = (yGracza * -1 +10) * 10 + '%';
@@ -137,34 +163,109 @@ function renderowanie()
 
 }
 
-document.addEventListener('keydown', klawiszKlikniety);
+function upadek()
+{
+    var mango = Math.round(yGracza);
 
+    if (mango == yGracza)
+    {
+        yGracza = Math.round(yGracza);
+    }
+
+    console.log(yGracza);
+
+    if (bloki[yGracza][xGracza] == -1)
+    {
+        yGracza++;
+        return true;
+    }
+    else { return false;}
+}
+
+function upadek2()
+{
+    if (bloki[Math.round(yGracza)][xGracza] == -1)
+    {
+        return true;
+    }
+    else { return false;}
+}
+
+document.addEventListener('keydown', klawiszKlikniety);
 
 function klawiszKlikniety()
 {
-
-    if (event.key == 'a') 
-    { 
-        if (bloki[yGracza-1][xGracza -1] == -1 && bloki[yGracza-2][xGracza -1] == -1) {xGracza--;}
-    }
-    else if (event.key == 'd') 
+    if (Math.floor(yGracza) != yGracza)
     {
-        if (bloki[yGracza-1][xGracza +1] == -1 && bloki[yGracza-2][xGracza +1] == -1) {xGracza++;}
+        yGracz = Math.floor(yGracza) -1;
     }
+
+
+    yGracza = Math.round(yGracza);
+
+    if (event.key == 'a')  {  if (bloki[yGracza-1][xGracza -1] == -1 && bloki[yGracza-2][xGracza -1] == -1) {xGracza--;} usuwaniePointera();}
+    else if (event.key == 'd')  { if (bloki[yGracza-1][xGracza +1] == -1 && bloki[yGracza-2][xGracza +1] == -1) {xGracza++;} usuwaniePointera(); }
     else if (event.key == 'w') 
     {
+        usuwaniePointera();
+
         console.log('Skok!');
-        setTimeout(() => {yGracza--; renderowanie();}, 500);
+        for (var i = 0; i < 10; i++) 
+        {
+            setTimeout(() => { yGracza  = yGracza - 0.10; renderowanie();}, i * 30);
+        }
+        
+        if (upadek2())
+        {
+            for (var i = 0; i < 10; i++) 
+            {
+                setTimeout(() => { yGracza  = yGracza + 0.10; renderowanie();}, i * 30 + 150);
+            }
+        }    
     }
 
-    for (var i = 0; i < 10; i++) {
+    else if (event.key ==  'ArrowLeft')
+    {
+        xPointer++;
+        zmienianiePozycjiPointera(yPointer, xPointer);
+    }
+    else if (event.key == 'ArrowRight')
+    {
+        xPointer--;
+        zmienianiePozycjiPointera(yPointer, xPointer);
+    }
+    else if (event.key == 'ArrowUp')
+    {
+        yPointer--;
+        zmienianiePozycjiPointera(yPointer, xPointer);
+    }
+    else if (event.key == 'ArrowDown')
+    {
+        yPointer++;
+        zmienianiePozycjiPointera(yPointer, xPointer);
+    }
+    else if (event.key == 'Enter')
+    {
+        console.log('Umieszczanie bloku na pozycji: ' + yPointer + ', ' + ((xPointer * -1) + 9));
+        bloki[yPointer][(xPointer * -1) + 9 + xGracza -4 ] = zrespBlok('txt/dirt.png');
+
+    }
+
+
+
+
+    while (true) {
         if (!upadek()) {break;}
     }
 
     generowanieSwiata();
     renderowanie();
 }
-
+function klikniecieMysza()
+{
+    const xClient = event.clientX;
+    const yClient = event.clientY;
+}
 
 //poStarcie
 for (var meow = 0; meow < 10; meow++) {
@@ -181,7 +282,7 @@ for (var y = 0; y < 10; y++)
 {
     bloki[y][3] = zrespBlok('txt/bedrock.png');
 }
-
+klawiszKlikniety();
 
 renderowanie();
 
